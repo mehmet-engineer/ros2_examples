076521ee9ac99720e654dec0394ce07c3c301fcf
mehmet-engineer <kahramanxx10@gmail.com>
2023-10-25 10:30:05 +0300

    gazebo package added

---
+++ gazebo_robot_sim/launch/bringup_robot.launch.py
@@ -0,0 +1,122 @@
+import os
+from launch import LaunchDescription
+from launch_ros.descriptions import ParameterValue
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, SetEnvironmentVariable
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory, get_package_prefix
+
+
+def generate_launch_description():
+
+    # ARGUMENTS ---------------------------------------------------------------
+
+    rviz_arg = DeclareLaunchArgument("rviz_gui", default_value="false",
+                                       description="Flag to enable rviz")
+
+    # current package path
+    curr_pkg_share_path = get_package_share_directory("gazebo_robot_sim")
+    robot_description_pkg_share_path = get_package_share_directory("ur5_description")
+
+    # Rviz config path
+    rviz_config_path = PathJoinSubstitution(
+        [robot_description_pkg_share_path, "rviz", "view_robot.rviz"]
+    )
+
+    robot_description = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [robot_description_pkg_share_path, "urdf", "ur5.urdf.xacro"]
+            ),
+            " ",
+            "name:=ur5"
+        ]
+    )
+
+    # NODES -----------------------------------------------------------------
+
+    robot_state_publisher_node = Node(
+        name="robot_state_publisher",
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[{"robot_description": ParameterValue(robot_description, value_type=str)}]
+    )
+    joint_state_publisher_node = Node(
+        name="joint_state_publisher",
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        output="screen"
+    )
+    rviz_node = Node(
+        name="rviz2",
+        package="rviz2",
+        executable="rviz2",
+        output="screen",
+        arguments=["-d", rviz_config_path],
+        condition=IfCondition(LaunchConfiguration("rviz_gui"))
+    )
+
+    # GAZEBO -----------------------------------------------------------------
+
+    description_share = os.path.join(get_package_prefix("ur5_description"), "share")
+    gazebo_env_var = SetEnvironmentVariable("GAZEBO_MODEL_PATH", description_share)
+
+    gazebo_server = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory("gazebo_ros"), "launch", "gzserver.launch.py")
+        ),
+        launch_arguments={
+            'physics': '',   # default ode
+            'verbose': 'true',
+            'world': os.path.join(curr_pkg_share_path, "worlds", "empty.world"),
+            'init': 'true',
+            'factory': 'true',
+            'force_system': 'true',
+        }.items()
+    )
+
+    gazebo_client = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory("gazebo_ros"), "launch", "gzclient.launch.py")
+        ),
+        launch_arguments={
+            'verbose': 'true'
+        }.items()
+    )
+
+    spawn_robot = Node(
+        name="spawn_robot",
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        output="screen",
+        arguments=["-entity", "ur5", 
+                   "-topic", "robot_description",
+                   "-x", "0.0",
+                   "-y", "0.0",
+                   "-z", "0.5"]
+    )
+
+    # Return Launch Function -------------------------------------------------
+
+    return LaunchDescription(
+        [   
+            rviz_arg,
+            joint_state_publisher_node,
+            robot_state_publisher_node,
+            rviz_node,
+            gazebo_env_var,
+            gazebo_server,
+            gazebo_client,
+            spawn_robot
+        ]
+    )
