e96a2ca6ab3c7a12453482180aa484659beacbd6
mehmet-engineer <kahramanxx10@gmail.com>
2026-09-15 17:21:17 +0300

    gazebo fortress updated

--- README.md
+++ README.md
@@ -2,15 +2,42 @@
 
 ROS2 Humble examples with UR5 robot arm.
 
-*Author: Mehmet Kahraman / Date 04.04.2024*
+*Author: Mehmet Kahraman / Date 15.09.2026*
 
 Main Requirements:
 --
 - Ubuntu 22.04 Jammy
 - ROS 2 Humble Desktop
+- Ignition Gazebo Fortress
 
 Installation and ROS Packages:
 --
+
+Install those ros2 humble packages using apt
+```
+sudo apt install ros-humble-gz-ros2-control -y
+sudo apt install ros-humble-gz-ros2-control-demos -y
+sudo apt install ros-humble-ign-ros2-control -y
+sudo apt install ros-humble-ros-gz-bridge -y
+sudo apt install ros-humble-rqt* -y
+sudo apt install ros-humble-joint-state-publisher* -y
+sudo apt install ros-humble-launch-param-builder -y
+sudo apt install ros-humble-parameter-traits -y
+sudo apt install ros-humble-ros2-control -y
+sudo apt install ros-humble-ros2-controllers -y
+sudo apt install ros-humble-controller-interface -y
+sudo apt install ros-humble-joint-trajectory-controller -y
+sudo apt install ros-humble-joint-state-broadcaster -y
+sudo apt install ros-humble-gripper-controllers -y
+sudo apt install ros-humble-xacro -y
+sudo apt install ros-humble-realtime-tools -y
+sudo apt install ros-humble-hardware-interface -y
+sudo apt install ros-humble-control-toolbox -y
+sudo apt install ros-humble-filters -y
+sudo apt install ros-humble-ros2bag -y
+sudo apt install ros-humble-plotjuggler* -y
+sudo apt install ros-humble-kdl-parser* -y
+```
 
 Clone workspace, build and source it
 ```
@@ -22,29 +49,6 @@ git clone https://github.com/mehmet-engineer/ros2_examples
 cd ..
 colcon build
 source install/setup.bash
-```
-
-Install those ros2 humble packages using apt
-```
-sudo apt install ros-humble-gazebo*
-sudo apt install ros-humble-rqt*
-sudo apt install ros-humble-moveit*
-sudo apt install ros-humble-joint-state-publisher*
-sudo apt install ros-humble-launch-param-builder
-sudo apt install ros-humble-parameter-traits
-sudo apt install ros-humble-ros2-control
-sudo apt install ros-humble-ros2-controllers
-sudo apt install ros-humble-controller-interface
-sudo apt install ros-humble-joint-trajectory-controller
-sudo apt install ros-humble-joint-state-broadcaster
-sudo apt install ros-humble-gripper-controllers
-sudo apt install ros-humble-xacro
-sudo apt install ros-humble-realtime-tools
-sudo apt install ros-humble-hardware-interface
-sudo apt install ros-humble-control-toolbox
-sudo apt install ros-humble-filters
-sudo apt install ros-humble-ros2bag
-sudo apt install ros-humble-plotjuggler*
 ```
 
 Running Launches and Nodes:
@@ -61,8 +65,8 @@ Node example
 ros2 run my_cpp_package publisher_node
 ```
 
-Bringup robot on Gazebo
+Bringup robot on Gazebo Fortress
 ```
-ros2 launch ur5_description bringup_gazebo.launch.py rviz_gui:=true
+ros2 launch gazebo_robot_sim bringup_robot.launch.py
 ```
-![img](assets/ur5_gazebo.png)
+![img](assets/gz_fortress.png)
--- gazebo_robot_sim/launch/bringup_robot.launch.py
+++ gazebo_robot_sim/launch/bringup_robot.launch.py
@@ -1,106 +1,53 @@
 import os
 from launch import LaunchDescription
-from launch_ros.descriptions import ParameterValue
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, SetEnvironmentVariable
-from launch.conditions import IfCondition
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import (
     Command,
     FindExecutable,
-    LaunchConfiguration,
     PathJoinSubstitution,
 )
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory, get_package_prefix
+from launch_ros.descriptions import ParameterValue
+from ament_index_python.packages import get_package_share_directory
+
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 
 
 def generate_launch_description():
 
-    # ARGUMENTS ---------------------------------------------------------------
+    # -------------------------------------------------------------------------
+    # PACKAGE PATHS
+    # -------------------------------------------------------------------------
 
-    rviz_arg = DeclareLaunchArgument("rviz_gui", default_value="false",
-                                       description="Flag to enable rviz")
-
-    # current package path
-    curr_pkg_share_path = get_package_share_directory("gazebo_robot_sim")
-    robot_description_pkg_share_path = get_package_share_directory("ur5_description")
-
-    # Rviz config path
-    rviz_config_path = PathJoinSubstitution(
-        [robot_description_pkg_share_path, "rviz", "view_robot.rviz"]
+    robot_description_pkg = get_package_share_directory(
+        "ur5_description"
     )
 
-    robot_description = Command(
-        [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
-            PathJoinSubstitution(
-                [robot_description_pkg_share_path, "urdf", "ur5.urdf.xacro"]
-            ),
-            " ",
-            "name:=ur5"
-        ]
+    gazebo_robot_sim_pkg = get_package_share_directory(
+        "gazebo_robot_sim"
     )
 
-    # NODES -----------------------------------------------------------------
+    # -------------------------------------------------------------------------
+    # ROBOT DESCRIPTION
+    # -------------------------------------------------------------------------
 
-    robot_state_publisher_node = Node(
-        name="robot_state_publisher",
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        output="screen",
-        parameters=[{"robot_description": ParameterValue(robot_description, value_type=str)}]
-    )
-    rviz_node = Node(
-        name="rviz2",
-        package="rviz2",
-        executable="rviz2",
-        output="screen",
-        arguments=["-d", rviz_config_path],
-        condition=IfCondition(LaunchConfiguration("rviz_gui"))
-    )
+    robot_description = Command([
+        FindExecutable(name="xacro"),
+        " ",
+        PathJoinSubstitution([
+            robot_description_pkg,
+            "urdf",
+            "ur5_urdf.xacro"
+        ]),
+        " ",
+        "name:=ur5"
+    ])
 
-    # GAZEBO -----------------------------------------------------------------
-
-    description_share = os.path.join(get_package_prefix("ur5_description"), "share")
-    gazebo_env_var = SetEnvironmentVariable("GAZEBO_MODEL_PATH", description_share)
-
-    gazebo_server = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory("gazebo_ros"), "launch", "gzserver.launch.py")
-        ),
-        launch_arguments={
-            'physics': '',   # default ode
-            'verbose': 'true',
-            'world': os.path.join(curr_pkg_share_path, "worlds", "empty.world"),
-            'init': 'true',
-            'factory': 'true',
-            'force_system': 'true',
-        }.items()
-    )
-
-    gazebo_client = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory("gazebo_ros"), "launch", "gzclient.launch.py")
-        ),
-        launch_arguments={
-            'verbose': 'true'
-        }.items()
-    )
-
-    spawn_robot = Node(
-        name="spawn_robot",
-        package="gazebo_ros",
-        executable="spawn_entity.py",
-        output="screen",
-        arguments=["-entity", "ur5", 
-                   "-topic", "robot_description",
-                   "-x", "0.0",
-                   "-y", "0.0",
-                   "-z", "0.5"]
-    )
-
-    # ROS 2 CONTROL ----------------------------------------------------------
+    # -------------------------------------------------------------------------
+    # JOINT STATE BROADCASTER
+    # -------------------------------------------------------------------------
 
     joint_state_broadcaster = Node(
         name="joint_state_broadcaster",
@@ -114,6 +61,74 @@ def generate_launch_description():
         ]
     )
 
+    # -------------------------------------------------------------------------
+    # ROBOT STATE PUBLISHER
+    # -------------------------------------------------------------------------
+
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="screen",
+        parameters=[
+            {
+                "robot_description": ParameterValue(
+                    robot_description,
+                    value_type=str
+                )
+            }
+        ]
+    )
+
+    # -------------------------------------------------------------------------
+    # GAZEBO FORTRESS
+    # -------------------------------------------------------------------------
+
+    parent_dir = os.path.dirname(robot_description_pkg)
+
+    if 'GZ_SIM_RESOURCE_PATH' in os.environ:
+        os.environ['GZ_SIM_RESOURCE_PATH'] += os.pathsep + parent_dir
+    else:
+        os.environ['GZ_SIM_RESOURCE_PATH'] = parent_dir
+
+    gazebo_fortress = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("ros_gz_sim"),
+                "launch",
+                "gz_sim.launch.py"
+            )
+        ),
+        launch_arguments={
+            "gz_args": "-r " + os.path.join(
+                gazebo_robot_sim_pkg,
+                "worlds",
+                "empty.world"
+            )
+        }.items()
+    )
+
+    # -------------------------------------------------------------------------
+    # SPAWN ROBOT
+    # -------------------------------------------------------------------------
+
+    spawn_robot = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=[
+            "-name", "ur5",
+            "-topic", "robot_description",
+            "-x", "0.0",
+            "-y", "0.0",
+            "-z", "0.4"
+        ],
+        output="screen"
+    )
+
+    # -------------------------------------------------------------------------
+    # ROS2 CONTROL & JOINT TRAJECTORY CONTROLLER
+    # -------------------------------------------------------------------------
+
     joint_trajectory_controller = Node(
         name="joint_trajectory_controller",
         package="controller_manager",
@@ -126,20 +141,23 @@ def generate_launch_description():
         ]
     )
 
-    # Return Launch Function -------------------------------------------------
-
-    return LaunchDescription(
-        [   
-            rviz_arg,
-            rviz_node,
-            robot_state_publisher_node,
-            
-            gazebo_env_var,
-            gazebo_server,
-            gazebo_client,
-            spawn_robot,
-
-            joint_state_broadcaster,
-            joint_trajectory_controller,
-        ]
+    load_joint_trajectory_controller = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster,
+            on_exit=[joint_trajectory_controller],
+        )
     )
+
+    # -------------------------------------------------------------------------
+    # LAUNCH
+    # -------------------------------------------------------------------------
+
+    return LaunchDescription([
+        joint_state_broadcaster,
+        robot_state_publisher,
+
+        gazebo_fortress,
+        spawn_robot,
+
+        load_joint_trajectory_controller
+    ])
